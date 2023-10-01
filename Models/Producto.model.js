eb94292a3cd044b7a216d8b71d7166f00feec2e8
Camilo Rojas <camilo.rojas9723@gmail.com>
2023-10-01 14:09:03 -0500

    Correciones

--- Models/Producto.model.js
+++ Models/Producto.model.js
@@ -14,7 +14,7 @@ const productSchema = new mongoose.Schema(
             type: Number,
             require: true
         },
-        categoría:{
+        categoria:{
             type: String,
             require: true,
         },
